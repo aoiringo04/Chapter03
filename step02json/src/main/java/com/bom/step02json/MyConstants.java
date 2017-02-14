de80c8c3e855b0214a82909309d8efdb6c43a4b7
aoiringo04 <aoiringo04@naver.com>
2017-02-15 02:45:38 +0900

    Step02 JSON
    예제 진행중...

---
+++ step02json/src/main/java/com/bom/step02json/MyConstants.java
@@ -0,0 +1,9 @@
+package com.bom.step02json;
+
+/**
+ * Created by OBOM on 2017-02-15.
+ */
+
+public class MyConstants {
+    public static final String REQUEST_URL = "http://study.kimgura.net/members";
+}
