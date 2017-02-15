3a538f72d1f0689a49dd0befdd5d721e5d296e49
aoiringo04 <aoiringo04@naver.com>
2017-02-15 19:58:20 +0900

    2017.02.15. 에러 고치는 중

--- step02json/src/main/java/com/bom/step02json/MainActivity.java
+++ step02json/src/main/java/com/bom/step02json/MainActivity.java
@@ -1,8 +1,10 @@
 package com.bom.step02json;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -11,38 +13,47 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity
-        implements View.OnClickListener, Util.RequestListener {
-
+        implements View.OnClickListener, Util.RequestListener, AdapterView.OnItemClickListener {
     ListView listView;
+    MemberAdapter adapter;
+    List<MemberDto> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView = (ListView) findViewById(R.id.listView);
-        Button getBtn = (Button) findViewById(R.id.getMemberBtn);
+        listView=(ListView)findViewById(R.id.listView);
+        Button getBtn=(Button)findViewById(R.id.getMemberBtn);
         getBtn.setOnClickListener(this);
+        list=new ArrayList<>();
+        adapter=new MemberAdapter(this,
+                R.layout.listview_cell, list);
+        listView.setAdapter(adapter);
+        // ListView 에 아이템 클릭 리스너 등록하기
+        listView.setOnItemClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        // sendGetRequest(요청 id, 요청 url, 요청 parameter, 리스너)
+        // sendGetRequest(요청 id, 요청 url, 요청 parameter, 리스너 )
         Util.sendGetRequest(0, MyConstants.REQUEST_URL, null, this);
     }
 
     @Override
     public void onSuccess(int requestId, Map<String, Object> result) {
-        if(requestId == 0){
-            int code = (int) result.get("code");
-            switch(code){
+        if(requestId==0){
+            int code=(int)result.get("code");
+            switch (code){
                 case 200:
-                    // 응답되는 문자열은 json 이다.
-                    String data = (String) result.get("data");
-                    Toast.makeText(this, data, Toast.LENGTH_SHORT).show();
+                    //응답되는 문자열은 json 이다.
+                    String data=(String)result.get("data");
+                    Toast.makeText(this, data, Toast.LENGTH_SHORT ).show();
                     parseJson(data);
                     break;
             }
@@ -53,22 +64,38 @@ public class MainActivity extends AppCompatActivity
     public void onFail(int requestId, Map<String, Object> result) {
 
     }
-
+    //
     public void parseJson(String json){
-        try {
-            JSONObject jsonObj = new JSONObject(json);
-            JSONArray jsonArr = jsonObj.getJSONArray("members");
-            for(int i = 0; i < jsonArr.length(); i++){
-                // i 번째 JSONObject 객체를 얻어온다.
-                JSONObject tmp = (JSONObject) jsonArr.get(i);
-                int num = tmp.getInt("num"); // 번호
-                String name = tmp.getString("name"); // 이름
-                String addr = tmp.getString("addr"); // 주소
-                // 모델에 추가한다.
-            }
 
+        try {
+            JSONObject jsonObj=new JSONObject(json);
+            JSONArray jsonArr=jsonObj.getJSONArray("members");
+            for(int i=0; i<jsonArr.length(); i++){
+                // i 번째 JSONObject 객체를 얻어온다.
+                JSONObject tmp = (JSONObject)jsonArr.get(i);
+
+                int num=tmp.getInt("num"); //번호
+                String name=tmp.getString("name"); //이름
+                String addr=tmp.getString("addr"); //주소
+                //모델에 추가 한다.
+                MemberDto dto=new MemberDto(num, name, addr);
+                list.add(dto);
+            }
         } catch (JSONException e) {
-            Toast.makeText(this, "JSON 문서에 오류가 있습니다.", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "JSON 문서에 오류가 있습니다.",
+                    Toast.LENGTH_SHORT).show();
         }
+        //아답타에 알린다.
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent intent = new Intent(this, DetailActivity.class);
+        // 인텐트 객체에 MemberDto 객체를 담는다.
+        intent.putExtra("dto", list.get(position));
+        // 액티비티 시작 시키기
+        startActivity(intent);
+        MemberDto dto = (MemberDto) intent.getSerializableExtra("dto");
     }
 }
--- step03sharedpreference/src/main/java/com/bom/step03shardpreference/SettingActivity.java
+++ step03sharedpreference/src/main/java/com/bom/step03shardpreference/SettingActivity.java
@@ -33,17 +33,17 @@ public class SettingActivity extends AppCompatActivity {
         if (savedName.equals("empty")) { // 만일 비어 있다면
             return; // 메소드 종료
         }
-        //나머지 정보를 얻어와서 저장된 정보를 출력해준다.
+        // 나머지 정보를 얻어와서 저장된 정보를 출력해준다.
         name.setText(pref.getString("name", ""));
         email.setText(pref.getString("email", ""));
         phoneNumber.setText(pref.getString("phoneNumber", ""));
         password.setText(pref.getString("password", ""));
-        //성별 체크하기
+        // 성별 체크하기
         String gender = pref.getString("sex", "");
-        //라디어 버튼의 참조값을 얻어온다.
+        // 라디오 버튼의 참조값을 얻어온다.
         RadioButton btnMan = (RadioButton) findViewById(R.id.man);
         RadioButton btnWoman = (RadioButton) findViewById(R.id.woman);
-        //현재 성별에 따라서 다른 라디오 버튼을 체크하게 한다.
+        // 현재 성별에 따라서 다른 라디오 버튼을 체크하게 한다.
         if (gender.equals("남")) {
             btnMan.setChecked(true);
         } else {
@@ -53,14 +53,14 @@ public class SettingActivity extends AppCompatActivity {
 
     // 저장 버튼을 눌렀을 때 호출되는 메소드
     public void save(View v) {
-        //입력한 정보를 읽어온다.
+        // 입력한 정보를 읽어온다.
         String inputName = name.getText().toString();
         String inputEmail = email.getText().toString();
         String inputPhoneNumber = phoneNumber.getText().toString();
         String inputPassword = password.getText().toString();
-        //선택한 라디오 버튼의 아이디 값을 얻어온다.
+        // 선택한 라디오 버튼의 아이디 값을 얻어온다.
         int buttonId = radioGroup.getCheckedRadioButtonId();
-        //3항 연산자를 이용해서 성별에 관련된 문자열 얻어오기
+        // 3항 연산자를 이용해서 성별에 관련된 문자열 얻어오기
         String inputSex = buttonId == R.id.man ? "남" : "여";
         /*
          *  String inputSex;
@@ -70,20 +70,19 @@ public class SettingActivity extends AppCompatActivity {
 		 *  	inputSex="여";
 		 *  }
 		 */
-        //informaion.xml 문서를 사용할 준비를 한다.
-        SharedPreferences pref =
-                getSharedPreferences("information", MODE_PRIVATE);
-        //정보를 저장할수 있는 에디터 객체 얻어오기
+        // informaion.xml 문서를 사용할 준비를 한다.
+        SharedPreferences pref = getSharedPreferences("information", MODE_PRIVATE);
+        // 정보를 저장할수 있는 에디터 객체 얻어오기
         SharedPreferences.Editor editor = pref.edit();
-        //에디터 객체를 이용해서 key : value 의 쌍으로 정보를 저장한다.
+        // 에디터 객체를 이용해서 key : value 의 쌍으로 정보를 저장한다.
         editor.putString("name", inputName);
         editor.putString("email", inputEmail);
         editor.putString("phoneNumber", inputPhoneNumber);
         editor.putString("password", inputPassword);
         editor.putString("sex", inputSex);
-        //저장하기 => informaion.xml 문서에 저장된다.
+        // 저장하기 => informaion.xml 문서에 저장된다.
         editor.commit();
-        //알림 띄우기
+        // 알림 띄우기
         new AlertDialog.Builder(this)
                 .setMessage("저장하였습니다.")
                 .setNeutralButton("확인", null)
