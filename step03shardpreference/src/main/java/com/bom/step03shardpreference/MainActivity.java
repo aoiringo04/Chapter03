ff3afb7c264f42cf6f052c18efe335d9f3def413
aoiringo04 <aoiringo04@naver.com>
2017-02-15 12:49:09 +0900

    Step03 SharedPreference
    예제 완성

--- step03shardpreference/src/main/java/com/bom/step03shardpreference/MainActivity.java
+++ step03shardpreference/src/main/java/com/bom/step03shardpreference/MainActivity.java
@@ -1,13 +1,14 @@
 package com.bom.step03shardpreference;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,13 +38,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            // 설정 액티비티로 이동
+            startActivity(new Intent(this, SettingActivity.class));
             return true;
         }
 
