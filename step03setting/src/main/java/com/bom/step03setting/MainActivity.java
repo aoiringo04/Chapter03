b1fd7f74968079e888443e71ba7c2bcd492bc5e9
aoiringo04 <aoiringo04@naver.com>
2017-02-15 19:22:06 +0900

    Step03 Setting
    완성

--- step03setting/src/main/java/com/bom/step03setting/MainActivity.java
+++ step03setting/src/main/java/com/bom/step03setting/MainActivity.java
@@ -1,13 +1,14 @@
 package com.bom.step03setting;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
 
