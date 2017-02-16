d946ae3a0db2d9d40fd37a235ac1c9b9564b6f69
aoiringo04 <aoiringo04@naver.com>
2017-02-16 09:54:29 +0900

    Step05 SQLite 모듈 생성

---
+++ step05sqlite/src/main/java/com/bom/step05sqlite/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bom.step05sqlite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
